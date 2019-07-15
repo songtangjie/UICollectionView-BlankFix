ab2bd40e50a03d85890559c747602fa31b339bc4
WilliamSong <25247134@qq.com>
2019-07-15 11:37:21 +0800

    新添加文件
    用于修复UICollectionView的reload方法调用多次导致cell空白问题。

---
+++ UICollectionView+Fix.h
@@ -0,0 +1,13 @@
+//
+//  UICollectionView+Fix.h
+//  XXX
+//
+//  Created by william on 2019/7/10.
+//  Copyright © 2019 xxx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UICollectionView (Fix)
+
+@end
